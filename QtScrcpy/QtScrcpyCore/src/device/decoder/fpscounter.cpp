dfc7474f5b1ec72264dc4247c98ba50419dd0704
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-06-15 21:30:44 +0700

    refactor: avoid FPS timer work while disabled

--- QtScrcpy/QtScrcpyCore/src/device/decoder/fpscounter.cpp
+++ QtScrcpy/QtScrcpyCore/src/device/decoder/fpscounter.cpp
@@ -1,65 +1,50 @@
-#include <QDebug>
 #include <QTimerEvent>
 
 #include "fpscounter.h"
 
-FpsCounter::FpsCounter(QObject *parent) : QObject(parent) {}
-
-FpsCounter::~FpsCounter() {}
+FpsCounter::FpsCounter(QObject *parent)
+    : QObject(parent)
+{
+}
 
 void FpsCounter::start()
 {
+    if (m_counterTimer != 0) return;
+
     resetCounter();
-    startCounterTimer();
+    m_counterTimer = startTimer(1000, Qt::PreciseTimer);
 }
 
 void FpsCounter::stop()
 {
-    stopCounterTimer();
+    if (m_counterTimer != 0) {
+        killTimer(m_counterTimer);
+        m_counterTimer = 0;
+    }
     resetCounter();
 }
 
-bool FpsCounter::isStarted()
+bool FpsCounter::isStarted() const noexcept
 {
-    return m_counterTimer;
+    return m_counterTimer != 0;
 }
 
-void FpsCounter::addRenderedFrame()
+void FpsCounter::addRenderedFrame() noexcept
 {
     m_rendered.fetch_add(1, std::memory_order_relaxed);
 }
 
-void FpsCounter::addSkippedFrame()
-{
-    m_skipped.fetch_add(1, std::memory_order_relaxed);
-}
-
 void FpsCounter::timerEvent(QTimerEvent *event)
 {
-    if (event && m_counterTimer == event->timerId()) {
-        m_curRendered = m_rendered.exchange(0, std::memory_order_acq_rel);
-        m_curSkipped = m_skipped.exchange(0, std::memory_order_acq_rel);
-        emit updateFPS(m_curRendered);
-        //qInfo("FPS:%d Discard:%d", m_curRendered, m_curSkipped);
+    if (event && event->timerId() == m_counterTimer) {
+        emit updateFPS(m_rendered.exchange(0, std::memory_order_acq_rel));
+        return;
     }
+
+    QObject::timerEvent(event);
 }
 
-void FpsCounter::startCounterTimer()
-{
-    stopCounterTimer();
-    m_counterTimer = startTimer(1000);
-}
-
-void FpsCounter::stopCounterTimer()
-{
-    if (m_counterTimer) {
-        killTimer(m_counterTimer);
-        m_counterTimer = 0;
-    }
-}
-
-void FpsCounter::resetCounter()
+void FpsCounter::resetCounter() noexcept
 {
     m_rendered.store(0, std::memory_order_release);
-    m_skipped.store(0, std::memory_order_release);
 }

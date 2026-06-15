4f6387633dab5d5d32a113ded122e27d3c5c405b
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-06-15 12:15:59 +0700

    refactor: define deterministic game input lifecycle

--- QtScrcpy/QtScrcpyCore/src/device/controller/inputconvert/inputconvertgame.h
+++ QtScrcpy/QtScrcpyCore/src/device/controller/inputconvert/inputconvertgame.h
@@ -1,107 +1,115 @@
 #ifndef INPUTCONVERTGAME_H
 #define INPUTCONVERTGAME_H
 
-#include <QPointF>
-#include <QVector>
 #include <QElapsedTimer>
+#include <QHash>
+#include <QPoint>
+#include <QPointF>
+#include <QTimer>
+#include <QVector>
+#include <array>
+#include <cstdint>
 
 #include "inputconvertnormal.h"
 #include "keymap.h"
 
 #define MULTI_TOUCH_MAX_NUM 10
+
 class InputConvertGame : public InputConvertNormal
 {
     Q_OBJECT
 public:
-    InputConvertGame(Controller *controller);
-    virtual ~InputConvertGame();
+    explicit InputConvertGame(Controller *controller);
+    ~InputConvertGame() override;
 
-    virtual void mouseEvent(const QMouseEvent *from, const QSize &frameSize, const QSize &showSize);
-    virtual void wheelEvent(const QWheelEvent *from, const QSize &frameSize, const QSize &showSize);
-    virtual void keyEvent(const QKeyEvent *from, const QSize &frameSize, const QSize &showSize);
-    virtual bool isCurrentCustomKeymap();
+    void mouseEvent(const QMouseEvent *from, const QSize &frameSize, const QSize &showSize) override;
+    void wheelEvent(const QWheelEvent *from, const QSize &frameSize, const QSize &showSize) override;
+    void keyEvent(const QKeyEvent *from, const QSize &frameSize, const QSize &showSize) override;
+    bool isCurrentCustomKeymap() override;
 
     void loadKeyMap(const QString &json);
 
 protected:
-    void updateSize(const QSize &frameSize, const QSize &showSize);
-    void sendTouchDownEvent(int id, QPointF pos);
-    void sendTouchMoveEvent(int id, QPointF pos);
-    void sendTouchUpEvent(int id, QPointF pos);
-    void sendTouchEvent(int id, QPointF pos, AndroidMotioneventAction action);
-    void sendKeyEvent(AndroidKeyeventAction action, AndroidKeycode keyCode);
-    QPointF calcFrameAbsolutePos(QPointF relativePos);
-    QPointF calcScreenAbsolutePos(QPointF relativePos);
-
-    // multi touch id
-    int attachTouchID(int key);
-    void detachTouchID(int key);
-    int getTouchID(int key);
-
-    // steer wheel
-    void processSteerWheel(const KeyMap::KeyMapNode &node, const QKeyEvent *from);
-
-    // click
-    void processKeyClick(const QPointF &clickPos, bool clickTwice, bool switchMap, const QKeyEvent *from);
-
-    // click mutil
-    void processKeyClickMulti(const KeyMap::DelayClickNode *nodes, const int count, const QKeyEvent *from);
-
-    // drag
-    void processKeyDrag(const QPointF &startPos, QPointF endPos, quint32 startDelay, float dragSpeed, const QKeyEvent *from);
-
-    // android key
-    void processAndroidKey(AndroidKeycode androidKey, const QKeyEvent *from);
-
-    // mouse
-    bool processMouseClick(const QMouseEvent *from);
-    bool processMouseMove(const QMouseEvent *from);
-    void moveCursorTo(const QMouseEvent *from, const QPoint &localPosPixel);
-    void mouseMoveStartTouch(const QMouseEvent *from);
-    void mouseMoveStopTouch();
-    void startMouseMoveTimer();
-    void stopMouseMoveTimer();
-
-    bool switchGameMap();
-    bool checkCursorPos(const QMouseEvent *from);
-    void hideMouseCursor(bool hide);
-
-    void getDelayQueue(const QPointF& start, const QPointF& end,
-                       const double& distanceStep, const double& posStepconst,
-                       quint32 lowestTimer, quint32 highestTimer,
-                       QVector<QPointF>& pathPos, QVector<quint32>& pathTimer);
-
-protected:
-    void timerEvent(QTimerEvent *event);
+    void timerEvent(QTimerEvent *event) override;
 
 private slots:
     void onSteerWheelTimer();
     void onDragTimer();
 
 private:
+    void updateSize(const QSize &frameSize, const QSize &showSize);
+
+    bool sendTouchDownEvent(int id, const QPointF &pos);
+    bool sendTouchMoveEvent(int id, const QPointF &pos);
+    bool sendTouchUpEvent(int id, const QPointF &pos);
+    bool sendTouchEvent(int id, const QPointF &pos, AndroidMotioneventAction action);
+    void sendKeyEvent(AndroidKeyeventAction action, AndroidKeycode keyCode);
+
+    QPointF clampRelativePos(const QPointF &pos) const;
+    QPointF calcFrameAbsolutePos(const QPointF &relativePos) const;
+    QPointF calcScreenAbsolutePos(const QPointF &relativePos) const;
+
+    int attachTouchID(int key);
+    void detachTouchID(int key);
+    void detachTouchIDByIndex(int id);
+    int getTouchID(int key) const;
+
+    void processSteerWheel(const KeyMap::KeyMapNode &node, const QKeyEvent *from);
+    void processKeyClick(const QPointF &clickPos, bool clickTwice, bool switchMap, const QKeyEvent *from);
+    void processKeyClickMulti(const KeyMap::DelayClickNode *nodes, int count, const QKeyEvent *from);
+    void processKeyDrag(const QPointF &startPos, const QPointF &endPos,
+                        quint32 startDelay, float dragSpeed, const QKeyEvent *from);
+    void processAndroidKey(AndroidKeycode androidKey, const QKeyEvent *from);
+
+    bool processMouseClick(const QMouseEvent *from);
+    bool processMouseMove(const QMouseEvent *from);
+    void moveCursorTo(const QMouseEvent *from, const QPoint &localPosPixel);
+    void mouseMoveStartTouch();
+    void mouseMoveStopTouch();
+    void startMouseMoveTimer();
+    void stopMouseMoveTimer();
+
+    bool switchGameMap();
+    void hideMouseCursor(bool hide);
+
+    void getDelayQueue(const QPointF &start, const QPointF &end,
+                       double distanceStep, double positionJitter,
+                       quint32 lowestTimer, quint32 highestTimer,
+                       QVector<QPointF> &pathPos, QVector<quint32> &pathTimer);
+
+    QPointF addJitter(const QPointF &pos);
+    void stopSteerWheel(bool releaseTouch = true);
+    void stopDrag(bool releaseTouch = true);
+    void releaseAllKeys();
+    void resetTouchState();
+    void invalidatePendingActions();
+
+private:
     QSize m_frameSize;
     QSize m_showSize;
     bool m_gameMap = false;
     bool m_needBackMouseMove = false;
-    int m_multiTouchID[MULTI_TOUCH_MAX_NUM] = { 0 };
+    bool m_processMouseMove = true;
+    bool m_cursorHidden = false;
+    std::uint64_t m_actionEpoch = 0;
+
+    std::array<int, MULTI_TOUCH_MAX_NUM> m_multiTouchID{};
+    std::array<QPointF, MULTI_TOUCH_MAX_NUM> m_touchPositions{};
+    std::array<QPoint, MULTI_TOUCH_MAX_NUM> m_lastAbsolutePositions{};
+    std::array<bool, MULTI_TOUCH_MAX_NUM> m_hasLastAbsolutePosition{};
+
     KeyMap m_keyMap;
 
-    bool m_processMouseMove = true;
-
-    // steer wheel
-    struct
-    {
-        // the first key pressed
+    struct SteerWheelState {
         int touchKey = Qt::Key_unknown;
         bool pressedUp = false;
         bool pressedDown = false;
         bool pressedLeft = false;
         bool pressedRight = false;
 
-        // for delay
-        struct {
+        struct DelayState {
             QPointF currentPos;
-            QTimer* timer = nullptr;
+            QTimer *timer = nullptr;
             QVector<QPointF> pathPos;
             QVector<quint32> pathTimer;
             int stepIndex = 0;
@@ -109,37 +117,25 @@ private:
         } delayData;
     } m_ctrlSteerWheel;
 
-    // mouse move
-    struct
-    {
+    struct MouseMoveState {
         QPointF lastConverPos;
-        QPointF lastPos = { 0.0, 0.0 };
+        QPointF lastPos;
         bool touching = false;
         int timer = 0;
         bool smallEyes = false;
-        int ignoreCount = 0;
         QElapsedTimer paceTimer;
     } m_ctrlMouseMove;
 
-    // for drag delay
-    struct {
+    struct DragState {
         QPointF currentPos;
-        QTimer* timer = nullptr;
+        QTimer *timer = nullptr;
         QVector<QPointF> pathPos;
         QVector<quint32> pathTimer;
         int stepIndex = 0;
         int pressKey = 0;
     } m_dragDelayData;
 
-    // Hash map untuk mencocokkan Jitter Posisi turun (Press) dan naik (Release)
-    QHash<int, QPointF> m_keyJitterMap; 
-
-    // Helper Jitter
-    QPointF addJitter(const QPointF& pos);
-
-    void stopSteerWheel();
-    void stopDrag();
-    void releaseAllKeys();
+    QHash<int, QPointF> m_keyJitterMap;
 };
 
 #endif // INPUTCONVERTGAME_H

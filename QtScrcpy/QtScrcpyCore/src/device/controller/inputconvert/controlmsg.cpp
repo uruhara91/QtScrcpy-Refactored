04ef2a08293d61924e4ff4356536ffda35575c75
uruhara91 <79046614+uruhara91@users.noreply.github.com>
2026-06-15 16:16:56 +0700

    fix: avoid mutating caller strings in control serialization

--- QtScrcpy/QtScrcpyCore/src/device/controller/inputconvert/controlmsg.cpp
+++ QtScrcpy/QtScrcpyCore/src/device/controller/inputconvert/controlmsg.cpp
@@ -7,7 +7,8 @@
 #define MAX(X, Y) ((X) > (Y) ? (X) : (Y))
 #define CLAMP(V, X, Y) MIN(MAX((V), (X)), (Y))
 
-ControlMsg::ControlMsg(ControlMsgType controlMsgType) : QScrcpyEvent(Control)
+ControlMsg::ControlMsg(ControlMsgType controlMsgType)
+    : QScrcpyEvent(Control)
 {
     m_data.type = controlMsgType;
 }
@@ -23,7 +24,10 @@ ControlMsg::~ControlMsg()
     }
 }
 
-void ControlMsg::setInjectKeycodeMsgData(AndroidKeyeventAction action, AndroidKeycode keycode, quint32 repeat, AndroidMetastate metastate)
+void ControlMsg::setInjectKeycodeMsgData(AndroidKeyeventAction action,
+                                         AndroidKeycode keycode,
+                                         quint32 repeat,
+                                         AndroidMetastate metastate)
 {
     m_data.injectKeycode.action = action;
     m_data.injectKeycode.keycode = keycode;
@@ -31,17 +35,13 @@ void ControlMsg::setInjectKeycodeMsgData(AndroidKeyeventAction action, AndroidKe
     m_data.injectKeycode.metastate = metastate;
 }
 
-void ControlMsg::setInjectTextMsgData(QString &text)
+void ControlMsg::setInjectTextMsgData(const QString &text)
 {
-    // write length (2 byte) + string (non nul-terminated)
-    if (CONTROL_MSG_INJECT_TEXT_MAX_LENGTH < text.length()) {
-        // injecting a text takes time, so limit the text length
-        text = text.left(CONTROL_MSG_INJECT_TEXT_MAX_LENGTH);
-    }
-    QByteArray tmp = text.toUtf8();
-    m_data.injectText.text = new char[tmp.length() + 1];
-    memcpy(m_data.injectText.text, tmp.data(), tmp.length());
-    m_data.injectText.text[tmp.length()] = '\0';
+    const QString clipped = text.left(CONTROL_MSG_INJECT_TEXT_MAX_LENGTH);
+    const QByteArray utf8 = clipped.toUtf8();
+    m_data.injectText.text = new char[utf8.size() + 1];
+    memcpy(m_data.injectText.text, utf8.constData(), utf8.size());
+    m_data.injectText.text[utf8.size()] = '\0';
 }
 
 void ControlMsg::setInjectTouchMsgData(
@@ -60,7 +60,10 @@ void ControlMsg::setInjectTouchMsgData(
     m_data.injectTouch.pressure = pressure;
 }
 
-void ControlMsg::setInjectScrollMsgData(QRect position, float hScroll, float vScroll, AndroidMotioneventButtons buttons)
+void ControlMsg::setInjectScrollMsgData(QRect position,
+                                        float hScroll,
+                                        float vScroll,
+                                        AndroidMotioneventButtons buttons)
 {
     m_data.injectScroll.position = position;
     m_data.injectScroll.hScroll = hScroll;
@@ -68,27 +71,26 @@ void ControlMsg::setInjectScrollMsgData(QRect position, float hScroll, float vSc
     m_data.injectScroll.buttons = buttons;
 }
 
-void ControlMsg::setGetClipboardMsgData(ControlMsg::GetClipboardCopyKey copyKey) 
+void ControlMsg::setGetClipboardMsgData(ControlMsg::GetClipboardCopyKey copyKey)
 {
     m_data.getClipboard.copyKey = copyKey;
 }
 
-void ControlMsg::setSetClipboardMsgData(QString &text, bool paste)
+void ControlMsg::setSetClipboardMsgData(const QString &text, bool paste)
 {
+    m_data.setClipboard.paste = paste;
+    m_data.setClipboard.sequence = 0;
+
     if (text.isEmpty()) {
         m_data.setClipboard.text = Q_NULLPTR;
         return;
     }
-    if (CONTROL_MSG_CLIPBOARD_TEXT_MAX_LENGTH < text.length()) {
-        text = text.left(CONTROL_MSG_CLIPBOARD_TEXT_MAX_LENGTH);
-    }
 
-    QByteArray tmp = text.toUtf8();
-    m_data.setClipboard.text = new char[tmp.length() + 1];
-    memcpy(m_data.setClipboard.text, tmp.data(), tmp.length());
-    m_data.setClipboard.text[tmp.length()] = '\0';
-    m_data.setClipboard.paste = paste;
-    m_data.setClipboard.sequence = 0;
+    const QString clipped = text.left(CONTROL_MSG_CLIPBOARD_TEXT_MAX_LENGTH);
+    const QByteArray utf8 = clipped.toUtf8();
+    m_data.setClipboard.text = new char[utf8.size() + 1];
+    memcpy(m_data.setClipboard.text, utf8.constData(), utf8.size());
+    m_data.setClipboard.text[utf8.size()] = '\0';
 }
 
 void ControlMsg::setDisplayPowerData(bool on)
@@ -98,7 +100,8 @@ void ControlMsg::setDisplayPowerData(bool on)
 
 void ControlMsg::setBackOrScreenOnData(bool down)
 {
-    m_data.backOrScreenOn.action = down ? AKEY_EVENT_ACTION_DOWN : AKEY_EVENT_ACTION_UP;
+    m_data.backOrScreenOn.action = down ? AKEY_EVENT_ACTION_DOWN
+                                        : AKEY_EVENT_ACTION_UP;
 }
 
 void ControlMsg::writePosition(QBuffer &buffer, const QRect &value)
@@ -112,23 +115,21 @@ void ControlMsg::writePosition(QBuffer &buffer, const QRect &value)
 quint16 ControlMsg::flostToU16fp(float f)
 {
     Q_ASSERT(f >= 0.0f && f <= 1.0f);
-    quint32 u = f * 0x1p16f; // 2^16
-    if (u >= 0xffff) {
-        u = 0xffff;
-    }
-    return (quint16)u;
+    quint32 u = f * 0x1p16f;
+    if (u >= 0xffff) u = 0xffff;
+    return static_cast<quint16>(u);
 }
 
 qint16 ControlMsg::flostToI16fp(float f)
 {
     Q_ASSERT(f >= -1.0f && f <= 1.0f);
-    qint32 i = f * 0x1p15f; // 2^15
+    qint32 i = f * 0x1p15f;
     Q_ASSERT(i >= -0x8000);
     if (i >= 0x7fff) {
-        Q_ASSERT(i == 0x8000); // for f == 1.0f
+        Q_ASSERT(i == 0x8000);
         i = 0x7fff;
     }
-    return (qint16)i;
+    return static_cast<qint16>(i);
 }
 
 QByteArray ControlMsg::serializeData()
@@ -153,25 +154,21 @@ QByteArray ControlMsg::serializeData()
         buffer.putChar(m_data.injectTouch.action);
         BufferUtil::write64(buffer, m_data.injectTouch.id);
         writePosition(buffer, m_data.injectTouch.position);
-        quint16 pressure = flostToU16fp(m_data.injectTouch.pressure);
+        const quint16 pressure = flostToU16fp(m_data.injectTouch.pressure);
         BufferUtil::write16(buffer, pressure);
         BufferUtil::write32(buffer, m_data.injectTouch.actionButtons);
         BufferUtil::write32(buffer, m_data.injectTouch.buttons);
-    } break;
+        break;
+    }
     case CMT_INJECT_SCROLL: {
         writePosition(buffer, m_data.injectScroll.position);
-        // Accept values in the range [-16, 16].
-        // Normalize to [-1, 1] in order to use sc_float_to_i16fp().
-        float hscrollNorm = m_data.injectScroll.hScroll / 16;
-        hscrollNorm = CLAMP(hscrollNorm, -1, 1);
-        float vscrollNorm = m_data.injectScroll.vScroll / 16;
-        vscrollNorm = CLAMP(vscrollNorm, -1, 1);
-        qint16 hScroll = flostToI16fp(hscrollNorm);
-        qint16 vScroll = flostToI16fp(vscrollNorm);
-        BufferUtil::write16(buffer, (quint16)hScroll);
-        BufferUtil::write16(buffer, (quint16)vScroll);
+        float hscrollNorm = CLAMP(m_data.injectScroll.hScroll / 16, -1, 1);
+        float vscrollNorm = CLAMP(m_data.injectScroll.vScroll / 16, -1, 1);
+        BufferUtil::write16(buffer, static_cast<quint16>(flostToI16fp(hscrollNorm)));
+        BufferUtil::write16(buffer, static_cast<quint16>(flostToI16fp(vscrollNorm)));
         BufferUtil::write32(buffer, m_data.injectScroll.buttons);
-    } break;
+        break;
+    }
     case CMT_BACK_OR_SCREEN_ON:
         buffer.putChar(m_data.backOrScreenOn.action);
         break;
@@ -186,7 +183,6 @@ QByteArray ControlMsg::serializeData()
             buffer.write(m_data.setClipboard.text, strlen(m_data.setClipboard.text));
         } else {
             BufferUtil::write32(buffer, 0);
-            // buffer.write(m_data.setClipboard.text, 0);
         }
         break;
     case CMT_SET_DISPLAY_POWER:
@@ -201,6 +197,7 @@ QByteArray ControlMsg::serializeData()
         qDebug() << "Unknown event type:" << m_data.type;
         break;
     }
+
     buffer.close();
     return byteArray;
 }
